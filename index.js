30b86e66865c67a5b7abfcc6b4604b6ab9f3fa54
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-01 14:03:08 -0700

    commit tree 00aa642191fc151f12de89f5e55b2a2c5b7e6abd

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+const products = [
+    { name: 'Shampoo', price: 4.99 },
+    { name: 'Donuts', price: 7.99 },
+    { name: 'Cookies', price: 6.49 },
+    { name: 'Bath Gel', price: 13.99 }
+];
+
+  function getTotalAmountForProducts(products) {
+    let totalPrice = 0;
+  
+    for (const product of products) {
+      totalPrice += product.price;
+    };
+  
+    return totalPrice;
+  }
+  
+//console.log(getTotalAmountForProducts(products)); // LOG: 33.46
+
+  // this is a reducer
+  const couponLocations = [
+    { room: 'Living room', amount: 5 },
+    { room: 'Kitchen', amount: 2 },
+    { room: 'Bathroom', amount: 1 },
+    { room: 'Master bedroom', amount: 7 }
+  ];
+  //reducers cycle through arrays
+  function ourReduce(arr, reducer, init) {
+      let accum = init;
+      for (const element of arr) {
+          accum = reducer(accum, element);
+      };
+      return accum;
+  }
+  
+  function couponCounter(totalAmount, location) {
+    return totalAmount + location.amount;
+  }
+  
+//console.log(ourReduce(couponLocations, couponCounter, 0)); // LOG: 15
+
+//simple example of a reducer
+const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
+// => 12
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(function(x, y) {
+    return x += y
+})
+console.log(totalBatteries)
+
